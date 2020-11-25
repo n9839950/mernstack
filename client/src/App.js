ed2e670b96b8f4c43d57710190f6481e3616edb4
Manan Patel <mananpatel@Manans-MBP.lan>
2020-11-25 16:15:53 +1000

    client and server side file system

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React ,{useEffect}from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import {useDispatch} from 'react-redux';
+import memories from './images/memories.jpeg';
+import {getPost} from './actions/posts';
+import Post from './components/Posts/Post';
+import Form from './components/Form/Form';
+import useStyles from './styles';
 
 const App = () => {
+    const dispatch = useDispatch();
+    const classes = useStyles();
+    useEffect(() => {
+        dispatch(getPost());
+    },[dispatch]);
+
+
     return(
-        <div>
-            <h1> App</h1>
-        </div>
+       <Container maxwidth="lg">
+       <AppBar className={classes.appBar}position="static" color="inherit">
+       <Typography className={classes.heading} varient="h2" align="center"> Memories</Typography>
+       <img className={classes.image} src={memories} alt="memories" height="60"/>
+       </AppBar>
+       <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="streach" spacing={3} >
+                    <Grid item xs={12} sm={7} >
+                        <Post/>
+                    </Grid>
+                    <Grid item xs={12} sm={4} >
+                        <Form/>
+                    </Grid>
+                </Grid>
+            </Container>
+       </Grow>
+       </Container>
 
     );
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors'; 
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
+app.use('/posts',postRoutes);
+// parse application/json
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb+srv://javascriptmernApp:Manan_0106@cluster0.4inqm.mongodb.net/<dbname>?retryWrites=true&w=majority' 
+const PORT = process.env.PORT || 5000;
+
+
+// connecting to the database
+mongoose.connect(CONNECTION_URL,{useNewUrlParser: true , useUnifiedTopology: true})
+    .then(()=> app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
+    .catch((error)=> console.error());
